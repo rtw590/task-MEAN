0ef4b6091612adab3896b0695857bf49147bb151
Rob Waller <r.t.waller@gmail.com>
2018-01-30 13:14:40 -0600

    stopped with same issue. main page not updating

--- client/src/app/components/tasks/tasks.component.ts
+++ client/src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'tasks',
-  templateUrl: `tasks.component.html`,
+  templateUrl: 'tasks.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+export class TasksComponent  { }
