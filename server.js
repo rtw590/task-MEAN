55411e35d9b04e13bdd34ada264735a727173aa6
Rob Waller <r.t.waller@gmail.com>
2018-01-30 11:18:06 -0600

    Done with backend. Started work on Angular

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-//  Bring in dependencies
 var express = require('express');
 var path = require('path');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var tasks = require('./routes/tasks');
@@ -10,21 +9,21 @@ var port = 3000;
 
 var app = express();
 
-//  View Engine
+// View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile)
+app.engine('html', require('ejs').renderFile);
 
-//  Set Static Folder
+// Set Static folder
 app.use(express.static(path.join(__dirname, 'client')));
 
-//  Body Parser Middleware
+// Body Parser MW
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', index);
 app.use('/api', tasks);
 
-app.listen(port, function() {
-    console.log('Server started on port '+port)
+app.listen(port, function(){
+    console.log('Server started on port ' + port);
 });
